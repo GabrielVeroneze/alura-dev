c040c3125cbac1d2dcca373fff51f07cd8221d4c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-24 20:56:56 -0300

    :sparkles: feat: Adiciona estilos Global e Reset, também utiliza componente de rotas

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '@/styles/Theme'
+import ResetStyles from '@/styles/Reset'
+import GlobalStyles from '@/styles/Global'
+import AppRoutes from '@/routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode></React.StrictMode>
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <ResetStyles />
+            <GlobalStyles />
+            <AppRoutes />
+        </ThemeProvider>
+    </React.StrictMode>
 )
