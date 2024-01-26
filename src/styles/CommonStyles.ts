1ba69fd781242b0da2e4d25be27ce3fdf90c982e
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-26 18:18:25 -0300

    :lipstick: feat: Cria estilo padrão para entradas de texto

--- src/styles/CommonStyles.ts
+++ src/styles/CommonStyles.ts
@@ -9,3 +9,20 @@ export const StyledIconButton = styled.button`
     transition-timing-function: ease-out;
     width: 48px;
 `
+
+export const StyledTextField = styled.input`
+    background-color: ${({ theme }) => theme.cores.fundo.secundaria + theme.cores.transparencia.moderada};
+    border: none;
+    border-radius: 0.5rem;
+    color: ${({ theme }) => theme.cores.texto.primaria};
+    font-size: ${({ theme }) => theme.fontes.tamanho.medio};
+    line-height: 1.5rem;
+    opacity: 0.64;
+    outline: unset;
+    padding: 1rem 0.875rem;
+    width: 100%;
+
+    &::placeholder {
+        color: ${({ theme }) => theme.cores.texto.primaria};
+    }
+`
