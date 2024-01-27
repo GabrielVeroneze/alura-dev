4e97c34da840765d86bdb182d74875d422b35ac6
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-26 21:37:20 -0300

    :sparkles: feat: Cria componente de cabeçalho

---
+++ src/components/Cabecalho/index.tsx
@@ -0,0 +1,34 @@
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { useMenuAberto } from '@/hooks/useMenuAberto'
+import { Header, MenuIcone } from './styled'
+import Buscador from '@/components/Buscador'
+import IconButton from '@/components/IconButton'
+import Perfil from '@/components/Perfil'
+import logo from '@/assets/images/logotipo.svg'
+
+const Cabecalho = () => {
+    const { menuAberto, setMenuAberto } = useMenuAberto()
+
+    const alternarMenuAberto = () => {
+        setMenuAberto(!menuAberto)
+    }
+
+    return (
+        <Header>
+            <img src={logo} alt="Logo do Alura Dev" />
+            <Buscador />
+            {menuAberto ? (
+                <MenuIcone>
+                    <IconButton icone={faXmark} onClick={alternarMenuAberto} />
+                </MenuIcone>
+            ) : (
+                <MenuIcone>
+                    <IconButton icone={faBars} onClick={alternarMenuAberto} />
+                </MenuIcone>
+            )}
+            <Perfil />
+        </Header>
+    )
+}
+
+export default Cabecalho
