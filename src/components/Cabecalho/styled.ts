c6a4f74cb75ca0be73a60e2728aa0b1023e12de0
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-26 21:39:46 -0300

    :sparkles: feat: Cria styled components para o Cabecalho

---
+++ src/components/Cabecalho/styled.ts
@@ -0,0 +1,30 @@
+import { device } from '@/styles/Breakpoints'
+import styled from 'styled-components'
+
+export const Header = styled.header`
+    align-items: center;
+    display: grid;
+    gap: 0.5rem;
+    grid-template-columns: 1fr max-content max-content;
+    height: 56px;
+    margin: 1rem ${({ theme }) => theme.paddings.lateral.pequeno} 2.5rem;
+
+    @media ${device.md} {
+        gap: 2.5rem;
+        grid-template-columns: max-content 1fr max-content;
+        margin: 2rem ${({ theme }) => theme.paddings.lateral.medio} 2.5rem;
+    }
+
+    @media ${device.xl} {
+        gap: 0;
+        grid-template-columns: 312px 1fr 312px;
+    }
+`
+
+export const MenuIcone = styled.span`
+    width: max-content;
+
+    @media ${device.xl} {
+        display: none;
+    }
+`
