de4c733aef661428f749db48af6d2cc6ea26558f
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-30 16:58:41 -0300

    :sparkles: feat: Cria styled component para o Botao

---
+++ src/components/Botao/styled.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+    background-color: ${({ theme }) => theme.cores.primaria};
+    border-radius: 0.5rem;
+    color: ${({ theme }) => theme.cores.texto.secundaria};
+    font-size: ${({ theme }) => theme.fontes.tamanho.medio};
+    height: 56px;
+    line-height: 1.5rem;
+    padding: 1rem;
+    width: 100%;
+
+    &:hover {
+        background-color: ${({ theme }) => theme.cores.secundaria};
+    }
+
+    &:active {
+        box-shadow: 0px 0px 0px 4px
+            ${({ theme }) => theme.cores.primaria + theme.cores.transparencia.alta};
+    }
+
+    &:focus {
+        background-color: ${({ theme }) => theme.cores.terciaria};
+    }
+`
