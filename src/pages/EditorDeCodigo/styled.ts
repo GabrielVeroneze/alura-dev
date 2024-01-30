d0f226731ee76d5256ff1827821fec82837b4086
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-29 22:40:25 -0300

    :sparkles: feat: Cria o grid da seção de editor

---
+++ src/pages/EditorDeCodigo/styled.ts
@@ -0,0 +1,23 @@
+import { device } from '@/styles/Breakpoints'
+import styled from 'styled-components'
+
+export const SecaoEditor = styled.section`
+    align-items: start;
+    display: grid;
+    gap: 2rem;
+    grid-template-areas:
+        'editor'
+        'botao'
+        'formulario';
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(3, max-content);
+
+    @media ${device.xl} {
+        gap: 2rem 2.5rem;
+        grid-template-areas:
+            'editor formulario'
+            'botao formulario';
+        grid-template-columns: 1fr calc(312px - 2.5rem);
+        grid-template-rows: max-content 1fr;
+    }
+`
