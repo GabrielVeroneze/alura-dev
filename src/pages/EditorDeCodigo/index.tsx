60cf1667a2dbeadb8236ceba9abda5417c98b303
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-02-01 18:13:30 -0300

    :sparkles: feat: Cria a lógica de aplicar o sintaxe highlight

--- src/pages/EditorDeCodigo/index.tsx
+++ src/pages/EditorDeCodigo/index.tsx
@@ -1,13 +1,24 @@
+import { useRef } from 'react'
 import { SecaoEditor } from './styled'
+import 'highlight.js/styles/tokyo-night-dark.css'
+import hljs from 'highlight.js'
 import CampoCodigo from './CampoCodigo'
 import BotaoVisualizar from './BotaoVisualizar'
 import Formulario from './Formulario'
 
 const EditorDeCodigo = () => {
+    const editorRef = useRef<HTMLElement>(null)
+
+    const aplicarHighlight = () => {
+        if (editorRef.current) {
+            hljs.highlightElement(editorRef.current)
+        }
+    }
+
     return (
         <SecaoEditor>
-            <CampoCodigo />
-            <BotaoVisualizar />
+            <CampoCodigo editorRef={editorRef} />
+            <BotaoVisualizar aplicarHighlight={aplicarHighlight} />
             <Formulario />
         </SecaoEditor>
     )
