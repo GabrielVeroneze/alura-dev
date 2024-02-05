54e7196f13d8cb9ece18720d93b513519aab708b
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-02-05 16:12:04 -0300

    :lipstick: feat: Aplica estilização com base no modelo do campo

--- src/components/CampoCodigo/styled.ts
+++ src/components/CampoCodigo/styled.ts
@@ -1,31 +1,31 @@
 import { device } from '@/styles/Breakpoints'
 import styled from 'styled-components'
 
-export const CorDeFundo = styled.div<{ $background: string }>`
+export const CorDeFundo = styled.div<{ $background: string; $modo: string }>`
     background: ${({ $background }) => $background};
     border-radius: 0.5rem;
     grid-area: editor;
-    padding: 2rem;
+    padding: ${({ $modo }) => $modo === 'editar' ? '2rem' : '1.5rem'};
 `
 
-export const CaixaDeTexto = styled.pre`
+export const CaixaDeTexto = styled.pre<{ $modo: string }>`
     background-color: #141414;
     background-image: url('images/code-buttons.svg');
-    background-position: 1rem 1rem;
+    background-position: ${({ $modo }) => $modo === 'editar' ? '1rem 1rem' : '0.75rem 0.75rem'};
     background-repeat: no-repeat;
     border-radius: 0.5rem;
     box-shadow: 0px 16px 24px 0px rgba(0, 0, 0, 0.24);
     color: ${({ theme }) => theme.cores.texto.primaria};
-    padding: 3.25rem 1rem 1rem;
+    padding: ${({ $modo }) => $modo === 'editar' ? '3.25rem 1rem 1rem' : '2.25rem 0.75rem 0.75rem'};
 `
 
-export const Editor = styled.code`
+export const Editor = styled.code<{ $modo: string }>`
     background-color: transparent;
     border: none;
     display: block;
     font-family: ${({ theme }) => theme.fontes.familia.secundaria};
     font-size: 0.875rem;
-    height: 360px;
+    height: ${({ $modo }) => $modo === 'editar' ? '360px' : '306px'};
     line-height: normal;
     outline: none;
     overflow-y: auto;
