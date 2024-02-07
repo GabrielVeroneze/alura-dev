f7f17f91f5e965d35f9dff2d0222f25650ade9ac
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-02-07 16:52:21 -0300

    :lipstick: feat: Remove o grid e adiciona o layout masonry responsivo

--- src/pages/Comunidade/styled.ts
+++ src/pages/Comunidade/styled.ts
@@ -1,19 +1,6 @@
-import { device } from '@/styles/Breakpoints'
+import { ResponsiveMasonry } from 'react-responsive-masonry'
 import styled from 'styled-components'
 
-export const Projetos = styled.section`
-    display: grid;
-    gap: 2rem;
-    grid-template-columns: minmax(0, 1fr);
-    grid-template-rows: repeat(auto-fill, minmax(578px, max-content));
-
-    @media ${device.md} {
-        grid-template-rows: repeat(auto-fill, minmax(518px, max-content));
-    }
-
-    @media ${device.xl} {
-        gap: 1.5rem;
-        grid-template-columns: repeat(2, 1fr);
-        grid-template-rows: repeat(auto-fill, minmax(464px, max-content));
-    }
-`
+export const Projetos = styled(ResponsiveMasonry).attrs({
+    columnsCountBreakPoints: { 0: 1, 1439: 2 },
+})``
