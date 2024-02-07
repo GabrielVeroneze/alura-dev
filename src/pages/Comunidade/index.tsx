06513bd66621cead7a9c485c39a1bb129ad4a4c1
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-02-07 16:50:46 -0300

    :sparkles: feat: Adiciona Masonry Layout

--- src/pages/Comunidade/index.tsx
+++ src/pages/Comunidade/index.tsx
@@ -1,5 +1,6 @@
 import { useBuscarProjetos } from '@/hooks/useBuscarProjetos'
 import { Projetos } from './styled'
+import Masonry from 'react-responsive-masonry'
 import CardProjeto from './CardProjeto'
 
 const Comunidade = () => {
@@ -7,9 +8,11 @@ const Comunidade = () => {
 
     return (
         <Projetos>
-            {projetos.map(projeto => (
-                <CardProjeto key={projeto.id} {...projeto} />
-            ))}
+            <Masonry columnsCount={2} gutter="1.5rem">
+                {projetos.map(projeto => (
+                    <CardProjeto key={projeto.id} {...projeto} />
+                ))}
+            </Masonry>
         </Projetos>
     )
 }
