2bb439d16f9ae6be60edd1660234ec96c642d9ed
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-02-09 15:21:17 -0300

    :sparkles: feat: Aplica o highlight no código dos projetos salvos

--- src/pages/Comunidade/CardProjeto/index.tsx
+++ src/pages/Comunidade/CardProjeto/index.tsx
@@ -1,12 +1,20 @@
+import { useLayoutEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faComment, faHeart } from '@fortawesome/free-solid-svg-icons'
+import { useAplicarHighlight } from '@/hooks/useAplicarHighlight'
 import { IProjeto } from '@/interfaces/IProjeto'
 import { Acoes, Botao, Card, Conteudo, Descricao, Info, Quantidade, Titulo } from './styled'
 import CampoCodigo from '@/components/CampoCodigo'
 import Perfil from '@/components/Perfil'
 
 const CardProjeto = ({ id, codigo, personalizacao, nome, descricao }: IProjeto) => {
+    const { codigoRef, aplicarHighlight } = useAplicarHighlight()
+
+    useLayoutEffect(() => {
+        aplicarHighlight()
+    }, [aplicarHighlight])
+
     return (
         <Card>
             <Link to={`/editar/${id}`}>
@@ -14,6 +22,7 @@ const CardProjeto = ({ id, codigo, personalizacao, nome, descricao }: IProjeto) 
                     modo="visualizar"
                     codigo={codigo}
                     personalizacao={personalizacao}
+                    referencia={codigoRef}
                 />
             </Link>
             <Conteudo>
