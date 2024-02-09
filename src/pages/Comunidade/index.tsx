78907656dffe340f802d2b156fe6eb1fcc54c4eb
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-02-09 19:59:56 -0300

    :boom: fix: Atualiza a utilização do hook que fornece os projetos

--- src/pages/Comunidade/index.tsx
+++ src/pages/Comunidade/index.tsx
@@ -1,16 +1,16 @@
-import { useBuscarProjetos } from '@/hooks/useBuscarProjetos'
+import { useManipularProjetos } from '@/hooks/useManipularProjetos'
 import { Projetos } from './styled'
 import Masonry from 'react-responsive-masonry'
 import CardProjeto from './CardProjeto'
 
 const Comunidade = () => {
-    const { projetos } = useBuscarProjetos()
+    const { projetos } = useManipularProjetos()
 
     return (
         <Projetos>
             <Masonry columnsCount={2} gutter="1.5rem">
                 {projetos.map(projeto => (
-                    <CardProjeto key={projeto.id} {...projeto} />
+                    <CardProjeto key={projeto.id} projeto={projeto} />
                 ))}
             </Masonry>
         </Projetos>
