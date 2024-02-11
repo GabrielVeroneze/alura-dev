3e9acecbf68a36b5d50f4140e0c941e245b96eed
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-02-10 22:58:35 -0300

    :sparkles: feat: Modifica a lógica de curtir um projeto

--- src/hooks/useCurtirProjeto.ts
+++ src/hooks/useCurtirProjeto.ts
@@ -1,25 +1,23 @@
-import { useState } from 'react'
 import { useManipularProjetos } from '@/hooks/useManipularProjetos'
 import { IProjeto } from '@/interfaces/IProjeto'
 
 export const useCurtirProjeto = () => {
-    const { editarProjeto } = useManipularProjetos()
-    const [projetoCurtido, setProjetoCurtido] = useState<boolean>(false)
+    const { editarProjeto, carregarProjetos } = useManipularProjetos()
 
     const curtirProjeto = (projeto: IProjeto) => {
-        setProjetoCurtido(!projetoCurtido)
-
-        if (projetoCurtido) {
-            projeto.quantidadeCurtidas -= 1
+        if (projeto.curtidas.curtido) {
+            projeto.curtidas.quantidade -= 1
         } else {
-            projeto.quantidadeCurtidas += 1
+            projeto.curtidas.quantidade += 1
         }
 
+        projeto.curtidas.curtido = !projeto.curtidas.curtido
+
         editarProjeto(projeto)
+        carregarProjetos()
     }
 
     return {
-        projetoCurtido,
         curtirProjeto,
     }
 }
