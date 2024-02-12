d0fd87343bb7fe3c03f926d7170d88c5dfb2d200
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-02-11 21:15:20 -0300

    :sparkles: feat: Cria átomo para projeto atual em edição

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,5 +1,27 @@
 import { atom } from 'recoil'
+import { IProjeto } from '@/interfaces/IProjeto'
 import { IPersonalizarEditor } from '@/interfaces/IPersonalizarEditor'
+
+export const projetoAtualState = atom<IProjeto>({
+    key: 'projetoAtualState',
+    default: {
+        id: '',
+        nome: '',
+        descricao: '',
+        codigo: '',
+        personalizacao: {
+            linguagem: 'javascript',
+            corFundo: '#6BD1FF',
+        },
+        curtidas: {
+            quantidade: 0,
+            curtido: false,
+        },
+        comentarios: {
+            quantidade: 0,
+        },
+    },
+})
 
 export const menuAbertoState = atom<boolean>({
     key: 'menuAbertoState',
