4db2fbcd0cf8aff1802b7ca2a3226e189d91e767
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-02-11 21:25:02 -0300

    :sparkles: feat: Modifica a lógica de definição do projeto atual

--- src/hooks/useProjetoAtual.ts
+++ src/hooks/useProjetoAtual.ts
@@ -1,2 +1,44 @@
+import { useEffect } from 'react'
+import { useRecoilState } from 'recoil'
+import { useParams } from 'react-router-dom'
+import { projetoAtualState } from '@/state/atom'
+import { IProjeto } from '@/interfaces/IProjeto'
+import http from '@/http'
+
 export const useProjetoAtual = () => {
+    const { id } = useParams()
+    const [projetoAtual, setProjetoAtual] = useRecoilState(projetoAtualState)
+
+    useEffect(() => {
+        if (id) {
+            http
+                .get<IProjeto>(`projetos/${id}`)
+                .then(resposta => {
+                    setProjetoAtual(resposta.data)
+                })
+        } else {
+            setProjetoAtual({
+                id: '',
+                nome: '',
+                descricao: '',
+                codigo: '',
+                personalizacao: {
+                    linguagem: 'javascript',
+                    corFundo: '#6BD1FF',
+                },
+                curtidas: {
+                    quantidade: 0,
+                    curtido: false,
+                },
+                comentarios: {
+                    quantidade: 0,
+                },
+            })
+        }
+    }, [id, setProjetoAtual])
+
+    return {
+        projetoAtual,
+        setProjetoAtual,
+    }
 }
