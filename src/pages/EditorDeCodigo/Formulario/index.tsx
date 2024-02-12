e27e9ea6b4e682560d4d1cf47fe2a9d1eb2fb928
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-02-11 21:51:18 -0300

    :sparkles: feat: Utiliza hook para manipular o formulário

--- src/pages/EditorDeCodigo/Formulario/index.tsx
+++ src/pages/EditorDeCodigo/Formulario/index.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import { usePersonalizarEditor } from '@/hooks/usePersonalizarEditor'
+import { useProjetoAtual } from '@/hooks/useProjetoAtual'
+import { useManipularFormulario } from '@/hooks/useManipularFormulario'
 import { CampoWrapper, Form, Input, InputCor, InputDescricao } from './styled'
 import TituloSidebar from '@/components/TituloSidebar'
 import ListaLinguagens from '@/components/ListaLinguagens'
 import Botao from '@/components/Botao'
 
 const Formulario = () => {
-    const { personalizacao, setPersonalizacao } = usePersonalizarEditor()
-    const [nome, setNome] = useState<string>('')
-    const [descricao, setDescricao] = useState<string>('')
-
-    const handleSubmit = (evento: React.FormEvent<HTMLFormElement>) => {
-        evento.preventDefault()
-    }
+    const { projetoAtual } = useProjetoAtual()
+    const { handleDadosChange, handlePersonalizarChange, handleFormularioSubmit } = useManipularFormulario()
 
     return (
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleFormularioSubmit}>
             <CampoWrapper>
                 <TituloSidebar>Seu projeto</TituloSidebar>
                 <Input
                     type="text"
                     placeholder="Nome do seu projeto"
-                    value={nome}
-                    onChange={evento => setNome(evento.target.value)}
+                    value={projetoAtual.nome}
+                    onChange={evento =>
+                        handleDadosChange('nome', evento.target.value)
+                    }
                 />
                 <InputDescricao
                     type="text"
                     placeholder="Descrição do seu projeto"
-                    value={descricao}
-                    onChange={evento => setDescricao(evento.target.value)}
+                    value={projetoAtual.descricao}
+                    onChange={evento =>
+                        handleDadosChange('descricao', evento.target.value)
+                    }
                 />
             </CampoWrapper>
             <CampoWrapper>
@@ -36,12 +35,9 @@ const Formulario = () => {
                 <ListaLinguagens />
                 <InputCor
                     type="color"
-                    value={personalizacao.corFundo}
-                    onChange={evento => 
-                        setPersonalizacao({
-                            ...personalizacao,
-                            corFundo: evento.target.value,
-                        })
+                    value={projetoAtual.personalizacao.corFundo}
+                    onChange={evento =>
+                        handlePersonalizarChange('corFundo', evento.target.value)
                     }
                 />
             </CampoWrapper>
