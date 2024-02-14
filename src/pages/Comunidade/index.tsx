27d05568a62b3283ffe0084cf96bba68fdeb1996
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-02-13 23:12:03 -0300

    :sparkles: feat: Adiciona renderização condicional
    Utiliza operador ternário para renderizar a lista de projetos, se existir os projetos. Ou  renderizar um aviso informativo, se não houver projetos.

--- src/pages/Comunidade/index.tsx
+++ src/pages/Comunidade/index.tsx
@@ -2,18 +2,23 @@ import { useManipularProjetos } from '@/hooks/useManipularProjetos'
 import { Projetos } from './styled'
 import Masonry from 'react-responsive-masonry'
 import CardProjeto from './CardProjeto'
+import NenhumProjeto from './NenhumProjeto'
 
 const Comunidade = () => {
     const { projetos } = useManipularProjetos()
 
     return (
-        <Projetos>
-            <Masonry columnsCount={2} gutter="1.5rem">
-                {projetos.map(projeto => (
-                    <CardProjeto key={projeto.id} projeto={projeto} />
-                ))}
-            </Masonry>
-        </Projetos>
+        projetos.length ? (
+            <Projetos>
+                <Masonry columnsCount={2} gutter="1.5rem">
+                    {projetos.map(projeto => (
+                        <CardProjeto key={projeto.id} projeto={projeto} />
+                    ))}
+                </Masonry>
+            </Projetos>
+        ) : (
+            <NenhumProjeto />
+        )
     )
 }
 
