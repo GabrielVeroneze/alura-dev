c51a264eef388c15595e567ad748aa16050a7693
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-02-15 22:30:30 -0300

    :sparkles: feat: Adiciona funcionalidade ao botão e ao select
    Cria estado para selecionar o formato de imagem e utiliza função do hook useCodigoEmImagem para converter o código em uma imagem no formato selecionado

--- src/pages/EditorDeCodigo/ExportarImagem/index.tsx
+++ src/pages/EditorDeCodigo/ExportarImagem/index.tsx
@@ -1,12 +1,21 @@
+import { useState } from 'react'
+import { useCodigoEmImagem } from '@/hooks/useCodigoEmImagem'
 import { BotaoConverter, ExportarWrapper, Formato, FormatosImagens } from './styled'
 
 const ExportarImagem = () => {
+    const { converterCodigo } = useCodigoEmImagem()
+    const [formatoSelecionado, setFormatoSelecionado] = useState<string>('png')
+
     return (
         <ExportarWrapper>
-            <BotaoConverter>
-                Converter para imagem
+            <BotaoConverter onClick={() => converterCodigo(formatoSelecionado)}>
+                Converter em imagem
             </BotaoConverter>
-            <FormatosImagens>
+            <FormatosImagens
+                onChange={evento =>
+                    setFormatoSelecionado(evento.target.value)
+                }
+            >
                 <Formato value="png">png</Formato>
                 <Formato value="jpg">jpg</Formato>
                 <Formato value="svg">svg</Formato>
