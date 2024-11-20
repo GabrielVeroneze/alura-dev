a8c4c7f4018ac27319a158c849b5a7af63acc02c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-11-19 21:50:30 -0300

    ✨ feat: Adiciona serviço para manipulação de projetos

---
+++ src/services/projetos.ts
@@ -0,0 +1,32 @@
+import { IProjeto } from '@/interfaces/IProjeto'
+import api from './api'
+
+export async function buscarProjetos() {
+    try {
+        const resposta = await api.get<IProjeto[]>('projetos')
+
+        return resposta.data
+    } catch {
+        throw new Error('Erro ao carregar os projetos.')
+    }
+}
+
+export async function criarProjeto(projeto: IProjeto) {
+    try {
+        const resposta = await api.post('projetos', projeto)
+
+        return resposta.status
+    } catch {
+        throw new Error('Erro ao cadastrar o projeto.')
+    }
+}
+
+export async function atualizarProjeto(projeto: IProjeto) {
+    try {
+        const resposta = await api.put(`projetos/${projeto.id}`, projeto)
+
+        return resposta.status
+    } catch {
+        throw new Error('Erro ao editar o projeto.')
+    }
+}
